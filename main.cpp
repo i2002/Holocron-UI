d1a7fa29f089d9fe2d8a7f05876443039ce22409
Tudor Butufei <tudor.butufei@gmail.com>
2022-10-24 04:08:52 +0300

    add sdl dependency

--- main.cpp
+++ main.cpp
@@ -1,6 +1,14 @@
+#include <SDL.h>
 #include <iostream>
 
-int main() {
-    std::cout << "Hello, world!\n";
+int main(int argv, char** args){
+    for(int i = 0; i < argv; i++) {
+        std::cout << args[i] << '\n';
+    }
+    if (SDL_Init(SDL_INIT_VIDEO) != 0){
+        std::cout << "SDL_Init Error: " << SDL_GetError() << std::endl;
+        return 1;
+    }
+    SDL_Quit();
     return 0;
 }
