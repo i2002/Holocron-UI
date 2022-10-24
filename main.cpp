2cfb968e41aa3fedbe0277e17017ed394e6edbce
Tudor Butufei <tudor.butufei@gmail.com>
2022-10-24 09:14:57 +0300

    window demo

--- main.cpp
+++ main.cpp
@@ -9,6 +9,30 @@ int main(int argv, char** args){
         std::cout << "SDL_Init Error: " << SDL_GetError() << std::endl;
         return 1;
     }
+
+    // Create an application window with the following settings:
+    SDL_Window *window = SDL_CreateWindow(
+            "An SDL2 window",                  // window title
+            SDL_WINDOWPOS_UNDEFINED,           // initial x position
+            SDL_WINDOWPOS_UNDEFINED,           // initial y position
+            640,                               // width, in pixels
+            480,                               // height, in pixels
+            SDL_WINDOW_OPENGL                  // flags - see below
+    );
+
+    // Check that the window was successfully created
+    if (window == NULL) {
+        // In the case that the window could not be made...
+        std::cout << "Could not create window: %s\n" << SDL_GetError();
+        return 1;
+    }
+
+    // The window is open: could enter program loop here (see SDL_PollEvent())
+    SDL_Delay(3000);  // Pause execution for 3000 milliseconds, for example
+
+    // Close and destroy the window
+    SDL_DestroyWindow(window);
+
     SDL_Quit();
     return 0;
 }
