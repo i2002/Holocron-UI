a1af243541ef632249cafa693cf698181fa55601
Tudor Butufei <tudor.butufei@gmail.com>
2022-12-02 14:58:27 +0200

    (window) initial refactor as widget derivate

--- src/Window.cpp
+++ src/Window.cpp
@@ -1,13 +1,13 @@
 #include "Window.h"
 
-Window::Window(const Window &w) :
-    title{w.title},
-    color{w.color},
-    widgets{w.widgets},
-    win{w.title, cen::iarea{1000, 500}},
-    renderer{win.make_renderer()}
-{
-}
+// Window::Window(const Window &w) :
+//     title{w.title},
+//     color{w.color},
+//     widgets{w.widgets},
+//     win{w.title, cen::iarea{1000, 500}},
+//     renderer{win.make_renderer()}
+// {
+// }
 
 Window::Window(const std::string &t, const cen::color &color) :
     win{t, cen::iarea{1000, 500}},
@@ -21,48 +21,38 @@ Window::Window(const std::string &t, const cen::color &color) :
 Window::~Window()
 {
     win.hide();
-    for(auto it : widgets) {
-        std::get<2>(it).destroy();
-    }
 }
 
-std::ostream& operator<<(std::ostream &os, const Window &w)
-{
-    os << "Window with title: \"" << w.title <<"\" and widgets: {\n";
-    for(auto it: w.widgets) {
-        os << "- at x: " << std::get<0>(it) << " and y: " << std::get<1>(it) << " " << std::get<2>(it);
-    }
-    os << "}\n";
-    return os;
-}
+// std::ostream& operator<<(std::ostream &os, const Window &w)
+// {
+//     os << "Window with title: \"" << w.title <<"\" and widgets: {\n";
+//     for(auto it: w.widgets) {
+//         os << "- at x: " << std::get<0>(it) << " and y: " << std::get<1>(it) << " " << std::get<2>(it);
+//     }
+//     os << "}\n";
+//     return os;
+// }
 
-Window& Window::operator=(const Window &other)
-{
-    title = other.title;
-    color = other.color;
-    widgets.clear();
-    for(auto it : other.widgets) {
-        widgets.push_back(it); // FIXME: shallow copy vs full copy?
-    }
+// Window& Window::operator=(const Window &other)
+// {
+//     title = other.title;
+//     color = other.color;
+//     widgets.clear();
+//     for(auto it : other.widgets) {
+//         widgets.push_back(it); // FIXME: shallow copy vs full copy?
+//     }
 
-    return *this;
-}
+//     return *this;
+// }
 
 void Window::render()
 {
     // window background color
     renderer.clear_with(color);
 
-    // render each widget at it's own position
-    for(auto it : widgets) {
-        std::get<2>(it).render(renderer, std::get<0>(it), std::get<1>(it));
-    }
+    // parent behaviour (render children)
+    Widget::render(renderer, {0, 0});
 
     // present rendered items
     renderer.present();
-}
-
-void Window::add_widget(Widget w, int x, int y)
-{
-    widgets.push_back({x, y, w});
 }
--- src/Window.h
+++ src/Window.h
@@ -9,25 +9,26 @@
 #include "Widget.h"
 
 
-class Window
+class Window : public Widget
 {
     std::string title;
     cen::color color;
-    std::vector<std::tuple<int, int, Widget>> widgets;
-    
+
     cen::window win;
     cen::renderer renderer;
 
 public:
-    Window(const Window &w);
+    // Window(const Window &w);
     explicit Window(const std::string &t = "Untitled window", const cen::color &color = cen::colors::white);
     ~Window();
-    
-    friend std::ostream& operator<<(std::ostream& os, const Window &w);
-    Window& operator=(const Window &other);
 
     void render();
-    void add_widget(Widget w, int x, int y);
+    
+    // friend std::ostream& operator<<(std::ostream& os, const Window &w);
+    // Window& operator=(const Window &other);
+
+protected:
+    void render_self(cen::renderer &, cen::ipoint) {};
 };
 
 #endif // WINDOW_H
