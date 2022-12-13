c2280e126681d195d3e44b548b6b0b028071348f
Tudor Butufei <tudor.butufei@gmail.com>
2022-12-13 03:13:40 +0200

    update tests

--- README.md
+++ README.md
@@ -31,17 +31,17 @@ Simple UI toolkit powered by SDL.
 #### Cerințe
 - [x] separarea codului din clase în `.h` (sau `.hpp`) și `.cpp`
 - [ ] moșteniri
-  - [ ] funcții virtuale (pure) apelate prin pointeri de bază, constructori virtuali (clone)
-  - [ ] apelarea constructorului din clasa de bază 
+  - [x] funcții virtuale (pure) apelate prin pointeri de bază, constructori virtuali (clone)
+  - [x] apelarea constructorului din clasa de bază 
   - [ ] smart pointers
   - [ ] `dynamic_cast`
-- [ ] suprascris cc/op= pentru copieri/atribuiri corecte, copy and swap
+- [x] suprascris cc/op= pentru copieri/atribuiri corecte, copy and swap
 - [ ] excepții
   - [ ] ierarhie proprie cu baza `std::exception` sau derivată din `std::exception`
   - [ ] utilizare cu sens: de exemplu, `throw` în constructor, `try`/`catch` în `main`
 - [ ] funcții și atribute `static`
-- [ ] STL
-- [ ] cât mai multe `const`
+- [x] STL
+- [x] cât mai multe `const`
 - [ ] la sfârșit: commit separat cu adăugarea unei noi derivate
 - [ ] tag de `git`: de exemplu `v0.2`
 
--- src/main.cpp
+++ src/main.cpp
@@ -2,6 +2,47 @@
 #include "DemoWidget.h"
 #include <iostream>
 
+// test functions
+void test_constructor_implementation()
+{
+    // static tests
+    Window wind1;
+    wind1.show();
+    Window wind2{"Titlu"};
+    DemoWidget wdg1{cen::iarea{20,20}};
+    wind2.add_child(&wdg1, cen::ipoint{10, 10});
+    wind2.show();
+    wind2.render();
+    std::cout << wind2;
+
+    // dynamic tests
+    std::cout << "1.\n";
+    Widget *w1 = new Window{"Hello"};
+    std::cout << *w1;
+
+    std::cout << "2.\n";
+    wind2.add_child(new DemoWidget{cen::iarea{30, 30}, cen::colors::aqua}, cen::ipoint{50, 50});
+    Widget *w2 = wind2.clone();
+    if(auto w = dynamic_cast<Window*>(w2)) {
+        w->render();
+    }
+    std::cout << *w2;
+
+    std::cout << "3.\n";
+    Window wind3{std::move(*(Window*)w2)}; // now w3 is empty
+    std::cout << wind3 << "\n" << *w2 << "\n";
+    if(auto w = dynamic_cast<Window*>(w2)) {
+        w->render();
+        w->hide();
+    }
+    wind3.hide();
+    std::cout << "---\n";
+
+    // window show delay
+    SDL_Delay(5000);
+}
+
+
 int main(int, char**)
 {
     // initialize SDL library
@@ -12,36 +53,6 @@ int main(int, char**)
     // std::cout << a;
     // a.run();
 
-    // static tests
-    Window wind1;
-    Window wind2{"Titlu"};
-    DemoWidget wdg1{cen::iarea{20,20}};
-    wind2.add_child(&wdg1, cen::ipoint{10, 10});
-    wind2.render();
-    std::cout << *((Widget*)&wind2); //QUESTION: any better way to do this?
-
-    // dynamic tests
-    std::cout << "1.\n";
-    Widget *w1 = new Window{"Hello"};
-    std::cout << *w1;
-
-    std::cout << "2.\n";
-    Widget *w2 = wind2.clone(); // QUESTION: why window not duplicated?
-    if(Window *w = dynamic_cast<Window*>(w2)) {
-        w->render();
-    }
-    std::cout << *w2;
-
-    std::cout << "3.\n";
-    Widget *w3{w2};
-    std::cout << *w3;
-
-    std::cout << "4.\n";
-    Widget *w4 = &wind2;
-    std::cout << *w4;
-    std::cout << "---\n";
-
-    // window show delay
-    SDL_Delay(5000);
+    test_constructor_implementation();
     return 0;
 }
